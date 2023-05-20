c70f5e4690550e99d0ef234f17e5cf9a847e39e2
shinyoonsik <yoonsik131@daum.net>
2023-05-20 18:59:31 +0900

    first commit 0520

---
+++ userA/src/main/java/com/transaction/userA/UserAApplication.java
@@ -0,0 +1,13 @@
+package com.transaction.userA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAApplication.class, args);
+	}
+
+}
---
+++ userA/src/test/java/com/transaction/userA/UserAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transaction.userA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
